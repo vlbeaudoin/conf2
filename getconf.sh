b0b4f5f8ef94ef365b2b0bc11bd20bf8680168dd
Victor Lacasse-Beaudoin <tatacom.devl@gmail.com>
2017-04-21 16:43:04 -0400

    Ajout de vérification si wget est présent
    Le script est mis dans une fonction, appelée si wget est présent.

--- getconf.sh
+++ getconf.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
-# TODO Vérifier si wget est présent
-# TODO si non présent, on demande si on veut l'installer
-# TODO si oui, yum install wget -y
+# Fonction appelée si wget est présent (voir dernière section du code pour l'appel de la fonction)
+function getconf {
+  # On va dans tmp!
+  cd /tmp
 
-# On va dans tmp!
-cd /tmp
+  # Obtention de la version la plus récente de conf2
+  wget https://github.com/vlbeaudoin/conf2/raw/master/releases/conf2-latest.tar.gz
 
-# Obtention de la version la plus récente de conf2
-wget https://github.com/vlbeaudoin/conf2/raw/master/conf2-latest.tar.gz
+  # Extraction des fichiers
+  tar -zxvf /tmp/conf2-latest.tar.gz
 
-# Extraction des fichiers
-tar -zxvf /tmp/conf2-latest.tar.gz
+  # Installation de conf
+  cd /tmp/conf2
 
-# Installation de conf
-cd /tmp/conf2
+  if [ ! -d src/ ]; then
+    echo "Répertoire src/ non-présent."
+    echo $'Êtes-vous dans le répertoire de conf2? Cela est nécessaire pour l\'installation'
+  else
+    # On déplace le script et le titre vers le répertoire /usr/bin
+    cp src/conf /usr/bin/conf -v
+    cp src/conf2.t /usr/bin/conf2.t -v
 
-if [ ! -d src/ ]; then
-  echo "Répertoire src/ non-présent."
-  echo $'Êtes-vous dans le répertoire de conf2? Cela est nécessaire pour l\'installation'
+    # On donne les droits en exécution à root sur le script et sur le titre en ASCII Art (vraiment beau, vraiment beau.)
+    chmod u+x /usr/bin/conf -v
+    chmod u+x /usr/bin/conf2.t -v
+
+    # Déplacement de la page MAN vers son répertoire
+    cp src/conf.1.gz /usr/share/man/man1/conf.1.gz -v
+
+    echo $'Execution de mandb...'
+    mandb -q
+  fi;
+
+  # Nettoyage des fichiers temporaires
+  echo "Nettoyage des fichiers temporaires..."
+  rm -rf /tmp/conf2*
+}
+# Vérifier si wget est présent
+testwget=$(rpm -qa | grep wget)
+
+if [ -z $testwget ];
+then
+  echo $'WGET non présent. Ce package est nécessaire pour l\'installation de CONF2.\nVeuillez installer wget et exécuter ce script. (yum install wget)'
+  exit 0
 else
-  # On déplace le script et le titre vers le répertoire /usr/bin
-  cp src/conf /usr/bin/conf -v
-  cp src/conf2.t /usr/bin/conf2.t -v
-
-  # On donne les droits en exécution à root sur le script et sur le titre en ASCII Art (vraiment beau, vraiment beau.)
-  chmod u+x /usr/bin/conf -v
-  chmod u+x /usr/bin/conf2.t -v
-
-  # Déplacement de la page MAN vers son répertoire
-  cp src/conf.1.gz /usr/share/man/man1/conf.1.gz -v
-
-  echo $'Execution de mandb...'
-  mandb -q
+  echo $'WGET détecté. Exécution normale du script...'
+  getconf
 fi;
-
-# Nettoyage des fichiers temporaires
-echo "Nettoyage des fichiers temporaires..."
-rm -rf /tmp/conf2*
